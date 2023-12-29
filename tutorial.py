197fc04a329c8c8f7cdb4f785c72c41da1617632
aqjshi <85421203+aqjshi@users.noreply.github.com>
2023-12-29 19:03:10 +0000

    finished training model on kirsten stewart facial exp.

--- tutorial.py
+++ tutorial.py
@@ -6,21 +6,24 @@ from urllib import request
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'runs/train/exp27/weights/last.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'runs/train/exp38/weights/best.pt', force_reload=True)
 
-# img  = os.path.join('data', 'images', 'awake.0a506b94-a41b-11ee-8ef2-f21a94e3088d.jpg')
+# img  = os.path.join('Twilight (2008) - I Know What You Are Scene _ Movieclips.mp4')
 # results =  model(img)
-# results.print()
 
-# %matplotlib inline
 # plt.imshow(np.squeeze(results.render()))
 # plt.show()
+# results.print()
+
+
+# %matplotlib inline
+
 # IMAGES_PATH = os.path.join('data', 'images')
 # labels = ['awake', 'drowsy']
 # number_imgs = 20
 
 
-cap =  cv2.VideoCapture(0)
+cap =  cv2.VideoCapture('Twilight (2008) - I Know What You Are Scene _ Movieclips.mp4')
 while cap.isOpened():
     ret, frame = cap.read()
     results = model(frame)
@@ -32,29 +35,29 @@ while cap.isOpened():
 cap.release()
 cv2.destroyAllWindows()
 
-# for label in labels:
-#     print('Collecting images for {}'.format(labels))
-#     time.sleep(5)
+for label in labels:
+    print('Collecting images for {}'.format(labels))
+    time.sleep(5)
     
-#     for img_num in range(number_imgs):
-#         print('Collecting images for {}, image number{}'.format(label, img_num))
-#         #webcam feed
-#         ret, frame = cap.read()
+    for img_num in range(number_imgs):
+        print('Collecting images for {}, image number{}'.format(label, img_num))
+        #webcam feed
+        ret, frame = cap.read()
 
 
-#         #Naming image path
-#         imgname = os.path.join(IMAGES_PATH, label+'.'+str(uuid.uuid1())+'.jpg')
+        #Naming image path
+        imgname = os.path.join(IMAGES_PATH, label+'.'+str(uuid.uuid1())+'.jpg')
 
-#         #writing images to file
-#         cv2.imwrite(imgname, frame)
+        #writing images to file
+        cv2.imwrite(imgname, frame)
 
-#         #render to screen
-#         cv2.imshow('Image Collection', frame)
+        #render to screen
+        cv2.imshow('Image Collection', frame)
 
-#         #delay between captures
-#         time.sleep(2)
+        #delay between captures
+        time.sleep(2)
 
-#         if cv2.waitKey(10) & 0xFF == ord('q'):
-#             break
-# cap.release()
-# cv2.destroyAllWindows()
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
